3b7af475b34470b319d0bf59e9e7827cf66cb06c
Jeremias Volker <volker@technologiestiftung-berlin.de>
2020-08-05 16:40:05 +0200

    Improve synchronization and handle state by server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const onChange = require('on-change');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -19,39 +20,77 @@ const server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
 
-let state = {
-  'exhibition': false
+const minutesBeforeNextToggle = 0.1
+const milliSecondsBeforeRestart = minutesBeforeNextToggle * 60 * 1000
+let intervalid
+
+let _store = {
+  toggles : {
+    exhibition : {
+      state : false,
+      countdownActive : false,
+      pgvalue : 0,
+      lastSwitchOn : null
+    }
+  }
 }
 
-function parseMessage(str) {
-  try {
-      return JSON.parse(str);
-  } catch (e) {
-      return str;
+
+const store = onChange(_store, function (path, value, previousValue) {
+	// console.log('this:', this);
+	// console.log('path:', path);
+	// console.log('value:', value);
+	// console.log('previousValue:', previousValue);
+
+  // TODO : handle this in receiving toggle changes
+  if (path == "toggles.exhibition.state" && value != previousValue) {
+
+    let i = 0
+    for (const plug of plugs) {
+      // console.log(plug, value)
+      setTimeout(() => plug.setPowerState(value), i * 1000); // staggered on/off
+      i++
+    }
+
+    if (value) startCountdown()
   }
+
+  // console.log("updating all devices")
+  io.emit('toggles', store.toggles)
+});
+
+function startCountdown() {
+  console.log("startCountdown")
+  const toggle = store.toggles.exhibition
+  toggle.lastSwitchOn = new Date();
+  toggle.countdownActive = true;
+
+  intervalid = setInterval(() => {
+
+    // console.log('toggle.lastSwitchOn ',toggle.lastSwitchOn)
+
+    toggle.pgvalue = (new Date() - toggle.lastSwitchOn) / milliSecondsBeforeRestart
+
+    if (toggle.pgvalue > 1) {
+      // console.log("stop")
+      clearInterval(intervalid)
+      toggle.countdownActive = false;
+    }
+    // console.log(toggle.pgvalue)
+  }, 50);
 }
 
 io.on('connection', (socket) => {
   console.log('socket.io client connected')
 
-  socket.emit('state', state)
+  socket.emit('toggles', store.toggles)
 
-  // ws.send(JSON.stringify(state));
+  socket.on('toggles', (data) => {
+    console.log("new toggles received")
 
-  socket.on('state', (data) => {
-    console.log("new state received")
-    // state = parseMessage(data)
-    // console.log(data)
-    state = data
-
-    let i = 0
-    for (const plug of plugs) {
-      setTimeout(() => plug.setPowerState(state.exhibition), i * 1000); // staggered on/off
-      i++
+    for (const key in data) {
+      store.toggles[key].state = data[key].state
     }
-
-    io.emit('state', state);
-
   });
 
 });
--- public/script.js
+++ public/script.js
@@ -1,76 +1,52 @@
-// Vue.use(VueNativeSock.default, 'ws://localhost:80', {
-//   reconnection: true, // (Boolean) whether to reconnect automatically (false)
-//   reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
-//   reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
-//   format: 'json'
-// })
-
-const socket = io();
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: 'localhost:80'
+  })
+);
 
 var vm = new Vue({
   el: '#app',
   vuetify: new Vuetify(),
-  watch: {
-    'state.exhibition' : function(state) {
-      this.startCountdown();
+  sockets: {
+    connect: function () {
+      console.log('socket connected')
     },
-    // state: function(_state) {
-    //   // this.$socket.sendObj(_state)
-    //   socket.emit('socket', _state)
-    // }
+    toggles: function (data) {
+      console.log('new toggles received')
+
+      this.toggles = data
+
+      // this._data = data
+      this.isFetching = false
+    }
   },
-  mounted : function () {
+  mounted() {
 
-    socket.on('state', data => {
-      // this.withoutWatchers(() => {
-        console.log('new state received')
-        this.state = data
-      // })
-    });
 
-    // this.$options.sockets.onmessage = (message) => {
-    //   this.state = JSON.parse(message.data)
-    //   // console.log()
-    // }
+    // throw new Error('Oops');
   },
   methods : {
-    startCountdown: function() {
-      this.lastSwitchOn = new Date();
-
-      this.intervalid = setInterval(() => {
-
-        this.pgvalue = (new Date() - this.lastSwitchOn) / this.milliSecondsBeforeRestart
-
-        if (this.pgvalue > 1) {
-          // console.log("stop")
-          clearInterval(this.intervalid)
-        }
-        // console.log(this.pgvalue)
-      }, 50);
-    },
-    sendState: function() {
-      socket.emit('state', this.state)
-      console.log('new state sent')
+    sendState: function(toggle) {
+      console.log('sending toggles')
+      this.$socket.emit('toggles', this.toggles)
     }
   },
   computed : {
-    milliSecondsBeforeRestart : function() {
-      return this.minutesBeforeNextToggle * 60 * 1000
-    },
-    countdownActive : function() {
-      return this.pgvalue > 0 && this.pgvalue < 1
+    mainToggle() {
+      return this.toggles.exhibition
     }
   },
-  data : function () {
+  data() {
     return {
-      state : {
-        exhibition : false
-      },
-      // message : '',
-      pgvalue : 0,
-      lastSwitchOn : null,
-      intervalid:'',
-      minutesBeforeNextToggle : 0.05
+      isFetching : true,
+      toggles : {
+        exhibition : {
+          state : false,
+          countdownActive : false,
+          pgvalue : 0,
+          lastSwitchOn : null
+        }
+      }
     }
   }
 })
