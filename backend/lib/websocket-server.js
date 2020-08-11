7910eae6e03e1579024c114e8aef0db0557d6bf5
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-08-11 16:59:59 +0200

    test for websocket handlers

--- backend/lib/websocket-server.js
+++ backend/lib/websocket-server.js
@@ -2,18 +2,19 @@ const socketIo = require('socket.io');
 const server = require('./server');
 const io = socketIo.listen(server);
 const store = require('./store');
+const togglesHandler = (data) => {
+  console.log("new toggles received");
+  for (const key in data) {
+    store.toggles[key].state = data[key].state;
+  }
+};
 module.exports = {
+  togglesHandler,
   io, connectionHandler: (socket) => {
     console.log("socket.io client connected");
 
     socket.emit("toggles", store.toggles);
 
-    socket.on("toggles", (data) => {
-      console.log("new toggles received");
-
-      for (const key in data) {
-        store.toggles[key].state = data[key].state;
-      }
-    });
+    socket.on("toggles", togglesHandler);
   }
 };
